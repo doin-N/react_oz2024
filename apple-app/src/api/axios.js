92646845490dd9e2fd57c27a12734f81535d4eeb
doin-N <tlatms777@gmail.com>
2024-05-21 16:55:44 +0900

    240521.02

---
+++ apple-app/src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+    params: {
+        api_key: "2d7466209ea48c0e03c55ca182ac2076",
+        language: "ko-KR"
+    }
+})
+
+export default instance;
---
+++ apple-app/src/api/requests.js
@@ -0,0 +1,12 @@
+const requests = {
+    fetchNowPlaying: "movie/now_playing",
+    fetchTrending: "/trending/all/week",
+    fetchTopRated: "/movie/top_rated",
+    fetchActionMovies: "/discover/mobie?with_genres=28",
+    fetchComedyMovies: "/discover/mobie?with_genres=35",
+    fetchHorrorMovies: "/discover/mobie?with_genres=27",
+    fetchRomanceMovies: "/discover/mobie?with_genres=10749",
+    fetchDocumentaries: "/discover/mobie?with_genres=99"
+}
+
+export default requests;
