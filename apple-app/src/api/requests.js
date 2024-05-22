6a3b5be8b6eb000eb1cb8b2e19ff30d4920a6974
doin-N <tlatms777@gmail.com>
2024-05-22 18:27:36 +0900

    240522

--- apple-app/src/api/requests.js
+++ apple-app/src/api/requests.js
@@ -2,11 +2,11 @@ const requests = {
     fetchNowPlaying: "movie/now_playing",
     fetchTrending: "/trending/all/week",
     fetchTopRated: "/movie/top_rated",
-    fetchActionMovies: "/discover/mobie?with_genres=28",
-    fetchComedyMovies: "/discover/mobie?with_genres=35",
-    fetchHorrorMovies: "/discover/mobie?with_genres=27",
-    fetchRomanceMovies: "/discover/mobie?with_genres=10749",
-    fetchDocumentaries: "/discover/mobie?with_genres=99"
+    fetchActionMovies: "/discover/movie?with_genres=28",
+    fetchComedyMovies: "/discover/movie?with_genres=35",
+    fetchHorrorMovies: "/discover/movie?with_genres=27",
+    fetchRomanceMovies: "/discover/movie?with_genres=10749",
+    fetchDocumentaries: "/discover/movie?with_genres=99"
 }
 
 export default requests;
